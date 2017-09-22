f68e1914562a47f3b1309655a9ff1c546274db49
Nate Taylor <nathantaylor89@gmail.com>
2017-09-21 21:10:52 -0600

    Add client_credentials flow

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
-require('dotenv').config()
-const querystring = require('querystring')
-const axios = require('axios')
+const { send } = require('micro')
 const { router, get } = require('microrouter')
 const redirect = require('micro-redirect')
+const fetch = require('node-fetch')
+const queryString = require('query-string')
 const uid = require('uid-promise')
 
-const spotifyURL = 'https://accounts.spotify.com'
-const spotifyRedirectURI = `${process.env.NOW_URL}/callback`
-const scopes = querystring.stringify(['playlist-modify-private', 'playlist-modify-public', 'user-library-read'].join(' '))
+const spotify = {
+  auth_uri: 'https://accounts.spotify.com',
+  redirect_uri: `${process.env.NOW_URL}/callback`,
+  client_id: process.env.SPOTIFY_CLIENT_ID,
+  client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+  scopes: queryString.stringify(['playlist-modify-private', 'playlist-modify-public', 'user-library-read'].join(' '))
+}
 const states = []
 
-const redirectWithQueryString = (res, data) => {
-  const location = `${process.env.REDIRECT_URI}?${querystring.stringify(data)}`
+const redirectWithQueryString = (res, data, uri = process.env.REDIECT_URI) => {
+  const location = `${uri}?${queryString.stringify(data)}`
   redirect(res, 302, location)
+}
+
+const fetchToken = async ({ body = {}, headers = {} }) => {
+  const response = await fetch(`${spotify.auth_uri}/api/token`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded', ...headers },
+    body: queryString.stringify({ ...body })
+  })
+  const data = await response.json()
+  return { status: response.status, data }
 }
 
 const login = async (req, res) => {
   const state = await uid(20)
   states.push(state)
-  const clientId = process.env.SPOTIFY_CLIENT_ID
-  const queryParams = `client_id=${clientId}&redirect_uri=${spotifyRedirectURI}&scopes=${scopes}&response_type=code&state=${state}`
-  redirect(res, 302, `${spotifyURL}/authorize?${queryParams}`)
+  const { scopes, redirect_uri, client_id } = spotify
+  const params = { response_type: 'code', scopes, redirect_uri, client_id }
+
+  redirectWithQueryString(res, params, `${spotify.auth_uri}/authorize`)
 }
 
 const callback = async (req, res) => {
@@ -34,26 +49,21 @@ const callback = async (req, res) => {
   } else {
     states.splice(states.indexOf(state), 1)
     try {
-      const { status, data } = await axios({
-        method: 'POST',
-        url: `${spotifyURL}/api/token`,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: querystring.stringify({
+      const { status, data } = await fetchToken({
+        body: {
           grant_type: 'authorization_code',
-          redirect_uri: spotifyRedirectURI,
-          client_id: process.env.SPOTIFY_CLIENT_ID,
-          client_secret: process.env.SPOTIFY_CLIENT_SECRET,
+          client_id: spotify.client_id,
+          client_secret: spotify.client_secret,
+          redirect_uri: spotify.redirect_uri,
           code
-        })
+        }
       })
 
       if (status === 200) {
         if (data.error) {
           redirectWithQueryString(res, { error: data.error_description })
         } else {
-          redirectWithQueryString(res, { access_token: data.access_token })
+          redirectWithQueryString(res, data)
         }
       } else {
         redirectWithQueryString(res, { error: 'Spotify server error' })
@@ -65,7 +75,18 @@ const callback = async (req, res) => {
   }
 }
 
+const token = async (req, res) => {
+  const auth = Buffer.from(`${spotify.client_id}:${spotify.client_secret}`).toString('base64')
+  const { data } = await fetchToken({
+    headers: { Authorization: `Basic ${auth}` },
+    body: { grant_type: 'client_credentials' }
+  })
+
+  send(res, 200, data)
+}
+
 module.exports = router(
   get('/login', login),
-  get('/callback', callback)
+  get('/callback', callback),
+  get('/token', token)
 )
